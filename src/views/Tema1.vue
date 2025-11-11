c9fc0232b021d792e53e1fb5bb1fd8904ecf5d94
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-11-11 14:21:52 -0500

    tema 1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,8 +5,284 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
- 
+      h1 Organización del espacio y ubicación de productos
+
+    .row.justify-content-center.align-items-center
+      .col-8.col-sm-6.col-lg-4.col-xl-3
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down" src="@/assets/curso/temas/tema1/1.png" alt="imagen de acompañamiento")
+      .col-lg-8.col-xl-7
+        p La organización del espacio en el punto de venta representa un elemento fundamental en la estrategia comercial del merchandising, ya que influye directamente en el comportamiento del consumidor, en la experiencia de compra y en los niveles de conversión. Una disposición eficiente permite maximizar el uso del área disponible, generar un flujo lógico de circulación, destacar productos estratégicos y reforzar la imagen del establecimiento.
+        .cajon.color-secundario.p-4.mb-3
+          p.mb-0 Según Martínez Cruz (s. f.), el diseño del espacio debe ser funcional, estético y adaptado al perfil del consumidor, teniendo en cuenta los principios de circulación, visibilidad, accesibilidad y jerarquía de productos. El layout, o diseño de la planta del local, debe diseñarse para orientar al cliente a recorrer la mayor parte del espacio disponible, facilitando el contacto visual con los productos de mayor margen.
+        p Entre los layouts más comunes se encuentran:
+        ul.lista-ul--color
+          li.d-flex
+            i.fa-solid.fa-check
+            | 
+            div
+              p.mb-0 <em>Grid</em> (rejilla): óptimo para supermercados, maximiza la eficiencia operativa.
+          li.d-flex
+            i.fa-solid.fa-check
+            | 
+            div
+              p.mb-0 <em>Loop</em> o <em>racetrack</em>: guía al consumidor por un circuito cerrado.
+          li.d-flex
+            i.fa-solid.fa-check
+            | 
+            div
+              p.mb-0 <em>Layout</em> libre o mixto: usado en tiendas especializadas, favorece la exploración.
+
+    .bg-fullwidth
+    .row.justify-content-center.align-items-center.z-2
+      .col-xl-10
+        .titulo-sexto.color-acento-contenido.mb-4
+          h5 Figura 1.
+          span <em>Grid</em> supertiendas 
+        img.d-sm-none.mb-2(data-aos="fade-up", src='@/assets/curso/temas/tema1/2-mob.png', alt='en la figura se muestra como es el layout tipo Grid (rejilla), desarrollado en un supermercado, supentienda o tienda.')
+        img.d-none.d-sm-block.mb-2(data-aos="fade-up", src='@/assets/curso/temas/tema1/2.png', alt='en la figura se muestra como es el layout tipo Grid (rejilla), desarrollado en un supermercado, supentienda o tienda.')
+        figcaption <strong>Fuente:</strong> SENA, 2025.
+
+    Separador
+    .row.justify-content-center
+      .col-xl-10
+        #t_1_1(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+          h3 1.1	Exhibiciones primarias y secundarias
+
+    .row.justify-content-center.align-items-center
+      .col-8.col-sm-6.col-lg-4.col-xl-3
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down" src="@/assets/curso/temas/tema1/3.png" alt="imagen de acompañamiento")
+      .col-lg-8.col-xl-7
+        p Las exhibiciones primarias son aquellas ubicaciones de alto impacto visual, situadas estratégicamente en entradas, cabeceras de góndola, vitrinas o islas. Son fundamentales para lanzar promociones, productos nuevos o destacar productos de alta rentabilidad. Estas zonas tienen alta afluencia de tránsito y permiten captar la atención del cliente desde el primer momento.
+        p Por otro lado, las exhibiciones secundarias cumplen una función de refuerzo. Se ubican en pasillos interiores, estanterías laterales, puntos de pago o espacios intermedios. Aunque tienen menor visibilidad directa, permiten activar compras por impulso, complementar la exhibición primaria y estimular el recuerdo del producto.
+        .p-4.rounded-4.bg-color-secundario-s10
+          .row.justify-content-center.align-items-center
+            .col-lg-2.d-none.d-lg-block
+              figure
+                img.w-60(data-aos="fade-down" src="@/assets/curso/temas/tema1/4.svg" alt="imagen de acompañamiento")
+            .col-lg-10
+              p.mb-0 Estas exhibiciones deben estar alineadas con el plan de marketing y los objetivos comerciales del punto de venta. El uso coherente de material POP, iluminación específica y señalética mejora la experiencia visual del consumidor (Editorial GG, 2016).
+
+    Separador
+    .row.justify-content-center
+      .col-xl-10
+        #t_1_2(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+          h3 1.2	Ubicación estratégica de productos
+
+    .row.justify-content-center.align-items-center
+      .col-8.col-sm-6.col-lg-4.col-xl-3
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down" src="@/assets/curso/temas/tema1/5.png" alt="imagen de acompañamiento")
+      .col-lg-8.col-xl-7
+        p La ubicación del producto dentro del punto de venta no es aleatoria. Responde a criterios técnicos definidos por el planograma, la estrategia comercial y el análisis de la rotación. Colocar productos a la altura de los ojos, por ejemplo, incrementa hasta en un 30 % la probabilidad de compra. Así mismo, los productos de alto margen suelen ubicarse en zonas calientes para asegurar su exposición prioritaria.
+        .p-4.bg-color-acento-contenido-a5.rounded-4.icon-before.icon-before--termometro.mb-3
+          p.mb-0 <strong>Las zonas calientes</strong>: áreas con mayor flujo de tránsito, deben utilizarse para colocar productos estratégicos como marcas propias, innovaciones o productos de rotación media. 
+        .p-4.bg-color-secundario-s10.rounded-4.icon-before.icon-before--termometro-frio
+          p.mb-0 <strong>Las zonas frías</strong>: espacios menos transitados, deben reactivarse mediante señalética atractiva, iluminación o reubicación de categorías.
+
+
+    Separador
+    #t_1_3(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 1.3	Escaparatismo
+
+    .bloque-texto-g.color-terciario.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/temas/tema1/6.jpg')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 El escaparatismo es una disciplina del merchandising visual que se encarga del diseño, planificación y montaje de los escaparates o vitrinas comerciales, con el objetivo de atraer la atención del público, comunicar mensajes de marca y estimular el deseo de compra. Va más allá de una simple exposición de productos: es una forma de comunicación visual estratégica, donde cada elemento desde la iluminación hasta la disposición de los objetos está pensado para generar un impacto emocional y sensorial en el consumidor.
+
+    .row.justify-content-center
+      .col-xl-10
+        .row.justify-content-center.align-items-center
+          .col-lg-2.d-none.d-lg-block
+            figure
+              img.w-60(data-aos="fade-down" src="@/assets/curso/temas/tema1/4.svg" alt="imagen de acompañamiento")
+          .col-lg-10
+            p.mb-0 El escaparatismo no solo cumple una función estética, sino también comercial y comunicativa, ya que traduce la identidad de la marca en una experiencia visual que seduce, informa y persuade. Por esta razón, es considerado uno de los pilares más importantes del merchandising aplicado al punto de venta.
+
+    .row.justify-content-center
+      .col-xl-10
+        h4(data-aos='zoom-in-right').mb-5
+          span.etiqueta-title Componentes
+
+
+
+    Separador
+    #t_1_3(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 1.4	Planogramas y categorías
+
+    img.z-2(data-aos="fade-up")(src="@/assets/curso/temas/tema1/30.png", alt="Imagen decorativa")
+
+    .row.justify-content-center.z-2.mb-5
+      .col-lg-10(data-aos="fade-down")
+        div.banner-text.bg-color-secundario-s20
+          p.mb-0 Un planograma es un esquema visual que muestra cómo deben organizarse los productos en los estantes o exhibidores de un punto de venta para optimizar el espacio, facilitar la compra y aumentar las ventas. Su elaboración implica analizar el comportamiento del consumidor, la rotación de productos y la estrategia comercial del establecimiento. Se diseña teniendo en cuenta aspectos como la altura de los anaqueles, la visibilidad del producto, las zonas calientes y frías, y las categorías de artículos, buscando siempre una presentación atractiva, ordenada y funcional (Levy & Weitz, 2017).
+
+    .row.justify-content-center
+      .col-xl-10
+        h4(data-aos='zoom-in-right').mb-5
+          span.etiqueta-title Proceso eficaz de creación de planogramas
+
+    .row.justify-content-center.mb-5
+      .col-xl-10
+        TabsA.color-primario
+          .tarjeta.bg-color-primario-p10.p-4.pt-5.h-100.d-flex.align-items-center(titulo="Recopilación de datos de SKU")
+            .row.align-items-center.justify-content-around
+              .col-12.col-sm-7
+                h5.mb-4 Recopilación de datos de SKU
+                p.mb-4 Se recolectan dimensiones y fotos de cada producto del inventario.
+                p.mb-4 <strong>Objetivo principal</strong>. Garantizar precisión y uso eficiente del espacio.
+              .col-4.col-sm-3
+                figure
+                  img(src="@/assets/curso/temas/tema1/31.png")
+
+          .tarjeta.bg-color-primario-p10.p-4.pt-5.h-100.d-flex.align-items-center(titulo="Registro de equipos y estanterías")
+            .row.align-items-center.justify-content-around
+              .col-12.col-sm-7
+                h5.mb-4 Registro de equipos y estanterías
+                p.mb-4 Se registran los tipos y tamaños de estanterías y accesorios en la tienda.
+                p.mb-4 <strong>Objetivo principal</strong>. Asegurar compatibilidad del planograma con la infraestructura.
+              .col-4.col-sm-3
+                figure
+                  img(src="@/assets/curso/temas/tema1/8.png")
+
+          .tarjeta.bg-color-primario-p10.p-4.pt-5.h-100.d-flex.align-items-center(titulo="Creación del CDT (categoría / cliente)	")
+            .row.align-items-center.justify-content-around
+              .col-12.col-sm-7
+                h5.mb-4 Creación del CDT (categoría / cliente)	
+                p.mb-4 Se analiza el perfil del cliente y su comportamiento por categoría.
+                p.mb-4 <strong>Objetivo principal</strong>. Mejorar la ubicación del producto según preferencias del cliente.
+              .col-4.col-sm-3
+                figure
+                  img(src="@/assets/curso/temas/tema1/9.png")
+
+          .tarjeta.bg-color-primario-p10.p-4.pt-5.h-100.d-flex.align-items-center(titulo="Diseño del planograma con software")
+            .row.align-items-center.justify-content-around
+              .col-12.col-sm-7
+                h5.mb-4 Diseño del planograma con software
+                p.mb-4 Se usa software especializado y se valida con gestores de categoría.
+                p.mb-4 <strong>Objetivo principal</strong>. Alinear el diseño con metas comerciales.
+              .col-4.col-sm-3
+                figure
+                  img(src="@/assets/curso/temas/tema1/10.png")
+
+          .tarjeta.bg-color-primario-p10.p-4.pt-5.h-100.d-flex.align-items-center(titulo="Formación del personal en visual merchandising")
+            .row.align-items-center.justify-content-around
+              .col-12.col-sm-7
+                h5.mb-4 Formación del personal en visual merchandising
+                p.mb-4 Se capacita al equipo sobre el uso del software y normas de exhibición.
+                p.mb-4 <strong>Objetivo principal</strong>. Asegurar ejecución coherente en tiendas.
+              .col-4.col-sm-3
+                figure
+                  img(src="@/assets/curso/temas/tema1/11.png")
+
+          .tarjeta.bg-color-primario-p10.p-4.pt-5.h-100.d-flex.align-items-center(titulo="Revisión del rendimiento del planograma")
+            .row.align-items-center.justify-content-around
+              .col-12.col-sm-7
+                h5.mb-4 Revisión del rendimiento del planograma
+                p.mb-4 Se analiza el desempeño del planograma usando datos de ventas y métricas.
+                p.mb-4 <strong>Objetivo principal</strong>. Realizar ajustes estratégicos basados en resultados.
+              .col-4.col-sm-3
+                figure
+                  img(src="@/assets/curso/temas/tema1/12.png")
+
+          .tarjeta.bg-color-primario-p10.p-4.pt-5.h-100.d-flex.align-items-center(titulo="Plan de comunicación e integración")
+            .row.align-items-center.justify-content-around
+              .col-12.col-sm-7
+                h5.mb-4 Plan de comunicación e integración
+                p.mb-4 Se coordina la implementación del planograma entre departamentos.
+                p.mb-4 <strong>Objetivo principal</strong>. Facilitar ejecución y actualización efectiva.
+              .col-4.col-sm-3
+                figure
+                  img(src="@/assets/curso/temas/tema1/13.png")
+
+    .row.justify-content-center.align-items-center.z-2.mb-5
+      .col-xl-10
+        .titulo-sexto.color-acento-contenido.mb-4
+          h5 Figura 2.
+          span Uso tecnología en la creación de planograma 
+        img.mb-2(data-aos="fade-up", src='@/assets/curso/temas/tema1/14.png', alt='la figura muestra el uso de tecnología en la creación de planogramas, se muestran dispositivos tecnológicos, tales como Tablet, computadora y celular con planogramas diseñados.')
+        figcaption <strong>Fuente:</strong> SENA, 2025.
+
+    .row.justify-content-center.align-items-center.z-2.mb-5
+      .col-xl-10
+        .titulo-sexto.color-acento-contenido.mb-4
+          h5 Figura 3.
+          span Ubicación de artículos en estantería
+        img.d-sm-none.mb-2(data-aos="fade-up", src='@/assets/curso/temas/tema1/15-mob.png', alt='La figura muestra como debe ser la ubicación de los productos en un estante según altura entre 30 cms y 60 cms.')
+        img.d-none.d-sm-block.mb-2(data-aos="fade-up", src='@/assets/curso/temas/tema1/15.png', alt='La figura muestra como debe ser la ubicación de los productos en un estante según altura entre 30 cms y 60 cms.')
+        figcaption <strong>Fuente:</strong> SENA, 2025.
+
+    .row.justify-content-center
+      .col-xl-10
+        h4(data-aos='zoom-in-right').mb-5
+          span.etiqueta-title Beneficios de implementar planogramas correctamente
+
+    .row.justify-content-center.align-items-center.factores-slider.z-2.mb-5
+      .d-none.d-xl-block.col-xl-4
+        figure
+          img(src='@/assets/curso/temas/tema1/17.png', alt='Imagen decorativa', data-aos="fade-up")
+
+      .col-xl-8.mb-3.mb-lg-0
+        SlyderF(columnas="col-lg-6")
+          .tarjeta.bg-color-gray.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img.factores-slider__icon(src='@/assets/curso/temas/tema1/18.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Mejor colocación del producto
+            p.mb-4.mb-5.text-center Incremento de ventas del 5 % al 15 %.
+
+          .tarjeta.bg-color-gray.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img.factores-slider__icon(src='@/assets/curso/temas/tema1/19.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Optimización del espacio
+            p.mb-4.text-center Aumento de ventas del 5 % al 10 %.
+          .tarjeta.bg-color-gray.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img.factores-slider__icon(src='@/assets/curso/temas/tema1/20.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Experiencia personalizada del cliente
+            p.mb-4.mb-5.text-center 62 % de consumidores prefieren tiendas con esta característica.
+
+          .tarjeta.bg-color-gray.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img.factores-slider__icon(src='@/assets/curso/temas/tema1/21.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Operaciones racionalizadas
+            p.mb-4.text-center Mejora de productividad hasta un 20 %.
+
+          .tarjeta.bg-color-gray.px-4.pt-5.factores-slider__tarjeta
+            .row.justify-content-center.mb-3
+              .col-7
+                img.factores-slider__icon(src='@/assets/curso/temas/tema1/22.png' alt='Ilustacion de acompañamiento')
+            .factores-slider__title-wrapper  
+              h4.factores-slider__title.mb-4 Decisiones basadas en datos
+            p.mb-4.text-center Margen de mejora hasta del 60 %. 
+
+
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-xl-10
+        .titulo-sexto.color-acento-contenido.mb-4
+          h5 Figura 4.
+          span Beneficios de implementar planogramas correctamente 
+        img.d-sm-none.mb-2(data-aos="fade-up", src='@/assets/curso/temas/tema1/16-mob.png', alt='La figura muestra como debe ser la ubicación de los productos en un estante según altura entre 30 cms y 60 cms.')
+        img.d-none.d-sm-block.mb-2(data-aos="fade-up", src='@/assets/curso/temas/tema1/16.png', alt='La figura muestra como debe ser la ubicación de los productos en un estante según altura entre 30 cms y 60 cms.')
+        figcaption <strong>Fuente:</strong> SENA, 2025.
+
+
+
+
+
+
+
 
 //-  .row.align-items-center.justify-content-center.z-2
 //-       .col-8.col-sm-6.col-xl-4
@@ -42,37 +318,7 @@
 
 
 
-//- TABS
-//- TabsA.color-primario
-//-           .tarjeta.bg-palmera.bg-color-secundario-s5.p-4.pt-5(titulo="Fórmula de cálculo")
-//-             h4.mb-4 Fórmula de cálculo
-//-             p.mb-5 Ecuación matemática que permite obtener el valor del indicador.
-//-             figure
-//-               img(src="@/assets/curso/temas/tema2/4.png")
-
-//-           .tarjeta.bg-palmera.bg-color-secundario-s5.p-4.pt-5(titulo="Unidad de medida")
-//-             h4.mb-4 Unidad de medida
-//-             p.mb-5 Forma en que se expresa el resultado obtenido.
-//-             figure
-//-               img(src="@/assets/curso/temas/tema2/5.png")
-
-//-           .tarjeta.bg-palmera.bg-color-secundario-s5.p-4.pt-5(titulo="Frecuencia de medición")
-//-             h4.mb-4 Frecuencia de medición
-//-             p.mb-5 Periodo de tiempo en el que se recopila y analiza la información.
-//-             figure
-//-               img(src="@/assets/curso/temas/tema2/6.png")
-
-//-           .tarjeta.bg-palmera.bg-color-secundario-s5.p-4.pt-5(titulo="Responsable")
-//-             h4.mb-4 Responsable
-//-             p.mb-5 Persona o equipo encargado de recopilar datos, calcular y analizar resultados.
-//-             figure
-//-               img(src="@/assets/curso/temas/tema2/7.png")
-
-//-           .tarjeta.bg-palmera.bg-color-secundario-s5.p-4.pt-5(titulo="Meta o valor objetivo")
-//-             h4.mb-4 Meta o valor objetivo
-//-             p.mb-5 Valor ideal o deseado que representa un desempeño exitoso.
-//-             figure
-//-               img(src="@/assets/curso/temas/tema2/8.png")
+        
 
 
 
